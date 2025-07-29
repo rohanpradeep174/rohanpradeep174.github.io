ef18d97d443231cb4f249b10689a4d14f11d5dee
Rohan P <rohanpradeep164@gmail.com>
2025-07-29 14:15:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function openModal(id) {
+    document.getElementById(id).style.display = 'block';
+}
+function closeModal(id) {
+    document.getElementById(id).style.display = 'none';
+}
+window.onclick = function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (event.target == modal) modal.style.display = 'none';
+    });
+}
